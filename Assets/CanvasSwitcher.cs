8d2eb4517f5509de058e06e7c68d33b24cac0212
U-Jae <77143213+Bungduck@users.noreply.github.com>
2024-07-23 00:55:38 +0900

    REF CanvasSwitcher
    Refactoring CanvasSwitcher.cs
    and connect public variance to next Scene(1, 2, 3, etc.)

--- Assets/CanvasSwitcher.cs
+++ Assets/CanvasSwitcher.cs
@@ -4,37 +4,39 @@ using UnityEngine.UI;
 
 public class CanvasSwitcher : MonoBehaviour
 {
-    public static CanvasSwitcher instance;
-
-    public Canvas canvasA; // 비활성화할 캔버스
-    public Canvas canvasB; // 활성화할 캔버스
-    public Button switchButton; // 캔버스를 전환할 버튼
+    public Canvas nextCanvas; // 활성화할 캔버스
     public OVRInput.Button controllerButton; // 컨트롤러에서 상호작용할 버튼
 
-    void Awake()
-    {
-        instance = this;
-    }
+    private Button canvasButton;
+    private Canvas curCanvas;
 
     void Start()
     {
+        canvasButton = GetComponent<Button>();
+        curCanvas = transform.parent.GetComponent<Canvas>();
         // 버튼의 onClick 이벤트에 메소드 연결
-        switchButton.onClick.AddListener(ToggleCanvases);
+        if (canvasButton != null)
+        {
+            canvasButton.onClick.AddListener(ToggleCanvases);
+        }
     }
 
     // 캔버스 전환 함수
     void ToggleCanvases()
     {
-        canvasA.gameObject.SetActive(false); // Canvas A 비활성화
-        canvasB.gameObject.SetActive(true);  // Canvas B 활성화
+        if(curCanvas != null)
+        {
+            curCanvas.gameObject.SetActive(false);
+        }
+        nextCanvas.gameObject.SetActive(true);  // Canvas B 활성화
     }
 
     void Update()
     {
-        if (controllerButton == 0) return;
-        if (OVRInput.GetDown(controllerButton) && switchButton != null)
+        if (controllerButton == OVRInput.Button.None) return;
+        if (OVRInput.GetDown(controllerButton) && canvasButton != null)
         {
-            switchButton.onClick.Invoke();
+            canvasButton.onClick.Invoke();
         }
     }
 }
