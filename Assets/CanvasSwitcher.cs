ef66dd7cc3d8a3c32c90f15e40fe2f7e591b0b75
U-Jae <77143213+Bungduck@users.noreply.github.com>
2024-07-23 00:29:37 +0900

    Fix tutorial scene(rightHand and etc.)
    connect controller and button script.
    and fix invisible right-hand ray

--- Assets/CanvasSwitcher.cs
+++ Assets/CanvasSwitcher.cs
@@ -1,11 +1,20 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CanvasSwitcher : MonoBehaviour
 {
+    public static CanvasSwitcher instance;
+
     public Canvas canvasA; // 비활성화할 캔버스
     public Canvas canvasB; // 활성화할 캔버스
     public Button switchButton; // 캔버스를 전환할 버튼
+    public OVRInput.Button controllerButton; // 컨트롤러에서 상호작용할 버튼
+
+    void Awake()
+    {
+        instance = this;
+    }
 
     void Start()
     {
@@ -18,5 +27,14 @@ public class CanvasSwitcher : MonoBehaviour
     {
         canvasA.gameObject.SetActive(false); // Canvas A 비활성화
         canvasB.gameObject.SetActive(true);  // Canvas B 활성화
+    }
+
+    void Update()
+    {
+        if (controllerButton == 0) return;
+        if (OVRInput.GetDown(controllerButton) && switchButton != null)
+        {
+            switchButton.onClick.Invoke();
+        }
     }
 }
